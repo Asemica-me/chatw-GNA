a2cadc23060ba080fdb02f5f3b8d76ffc45dbe2d
Lucrezia Pograri <ateossg5@gmail.com>
2025-05-24 13:38:36 +0200

    radar diagram

--- eval/radar_diagram.py
+++ eval/radar_diagram.py
@@ -9,9 +9,8 @@ def create_radar_diagram(
     line_color='blue',
     alpha=0.25,
     line_thickness=0.5,
-    label_distance=25,
-    label_font_size=20,
-    background_color='#f9ffe8'
+    label_distance=15,
+    label_font_size=15
 ):
 
     num_vars = len(categories)
@@ -23,24 +22,20 @@ def create_radar_diagram(
 
     fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
 
-    # Set the background color
-    fig.patch.set_facecolor(background_color)
-
     # Plot the data
     ax.plot(angles, values, color=line_color, linewidth=line_thickness)
     ax.fill(angles, values, color=fill_color, alpha=alpha)
 
     # Set the labels for each axis
     ax.set_xticks(angles[:-1])
-    ax.set_xticklabels(categories, size=label_font_size) # Apply font size here
+    ax.set_xticklabels(categories, size=label_font_size)
 
     # Set the radial limits and add grid lines
     ax.set_ylim(0, max_value)
     ax.set_yticks(np.arange(1, max_value + 1)) # Set radial ticks at 1, 2, 3, 4, 5
     ax.set_yticklabels([str(i) for i in np.arange(1, max_value + 1)], color="grey", size=15)
-    ax.tick_params(axis='y', pad=5) # Adjust padding for y-tick labels
-
-    # Adjust the distance of the x-axis labels (categories)
+    # Adjust the distance of the x and y axis labels
+    ax.tick_params(axis='y', pad=1)
     ax.tick_params(axis='x', pad=label_distance)
 
     ax.grid(True)
@@ -60,7 +55,6 @@ if __name__ == "__main__":
         line_color='blue',
         alpha=0.25,
         line_thickness=0.5,
-        label_distance=60,
-        label_font_size=20,
-        background_color='#f9ffe8'
+        label_distance=50,
+        label_font_size=15
     )
